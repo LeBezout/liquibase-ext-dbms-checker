903df4dab36d53c56e34d84bcd6d97ded65d98eb
Bezout <lebezout@gmail.com>
2022-12-29 18:51:45 +0100

    :white_check_mark: test rollback

--- src/test/java/liquibase/ext/dbms/MySQLSyntaxCheckerTest.java
+++ src/test/java/liquibase/ext/dbms/MySQLSyntaxCheckerTest.java
@@ -2,6 +2,7 @@ package liquibase.ext.dbms;
 
 import liquibase.Liquibase;
 import liquibase.changelog.ChangeSet;
+import liquibase.exception.LiquibaseException;
 import liquibase.exception.ValidationFailedException;
 import liquibase.ext.dbms.utils.LiquibaseUtils;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.io.StringWriter;
 import java.util.List;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -44,6 +46,22 @@ class MySQLSyntaxCheckerTest {
         try (Liquibase liquibase = liquibaseUtils.buildLiquibase()) {
             ValidationFailedException ex = Assertions.assertThrows(ValidationFailedException.class, () -> liquibase.update(""));
             liquibase.getLog().info(ex.getMessage());
+        }
+    }
+
+    @Test
+    void test_03_rollback() throws Exception {
+        try (Liquibase liquibase = liquibaseUtils.buildLiquibase()) {
+            // Update without checker
+            liquibase.update("");
+            liquibase.getLog().info("############ -------------------------------------------- FIN UPDATE");
+            // Enable checker and then rollback
+            liquibaseUtils.enableMySQLSyntaxChecker();
+            StringWriter output = new StringWriter();
+            LiquibaseException ex = Assertions.assertThrows(LiquibaseException.class, () -> liquibase.rollback("V0", "", output));
+            liquibase.getLog().info("############ -------------------------------------------- FIN ROLLBACK");
+            liquibase.getLog().info(ex.getMessage());
+            // liquibase.getLog().info(output.toString());
         }
     }
 }
